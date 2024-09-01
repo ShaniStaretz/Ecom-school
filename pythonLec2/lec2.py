737fb210d11129c356390ffc0cf118d6d8546fe2
ShaniStaretz <shakimi88@gmail.com>
2024-09-01 21:44:06 +0300

    upload lec2 files

---
+++ pythonLec2/lec2.py
@@ -0,0 +1,21 @@
+# START
+# x:int=888;
+# print(x, type(x));
+#
+# weight:float=float(input("enter your weight:\n"));
+# print("the weight-10 is:", weight-10)
+# year_birth:int=int(input("enter your birth year:\n"));
+# print("your birth year+10 is:", year_birth+10);
+
+# x: float = float(input("enter x:\n"));
+# y: float = float(input("enter y:\n"));
+# print("the diff is:", x - y);
+
+x: float = float(input("enter x:\n"));
+if x > 0:
+    # yes
+    print(x, "is positive")
+else:
+    print(x, "is negative")
+
+# END
